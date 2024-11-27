d5b7b354250889a1fde1bd54f431bfc97ed90916
Ali <aliadnani056@gmail.com>
2024-11-27 09:30:29 +0100

    added a list of AgentCommercial

--- src/main/java/org/example/crm/models/Supervisor.java
+++ src/main/java/org/example/crm/models/Supervisor.java
@@ -6,10 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 
 public class Supervisor extends Employe{
+    List<AgentCommercial> agents;
     public Supervisor(String cne, String nom, String prenom, String password) {
         super(cne, nom, prenom, password);
     }
