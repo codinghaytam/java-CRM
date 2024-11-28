99f7ab5c92b44251e86409fca4c77a20b3c53069
gold <m.soukrati7242@uca.ac.ma>
2024-11-29 00:54:29 +0100

    before merge 1

--- src/main/java/org/example/crm/HelloApplication.java
+++ src/main/java/org/example/crm/HelloApplication.java
@@ -10,7 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/hello-view.fxml"));
+        System.out.println(getClass().getResource("/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -18,6 +19,6 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
