a9b889f36ae9025828e7abfe14e05e8d922559fb
Ali <aliadnani056@gmail.com>
2024-12-09 17:02:24 +0100

    implemented the model for lead

--- src/main/java/org/example/crm/models/lead.java
+++ src/main/java/org/example/crm/models/lead.java
@@ -1,4 +1,20 @@
 package org.example.crm.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class lead {
+    private String leadId;
+    private String Name;
+    private String HeadQuarters;
+    private String Phone;
+    private String Email;
+    private String agentId;
 }
