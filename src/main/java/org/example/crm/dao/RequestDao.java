eeb44582ccc255551d5f71ec8ebfec1a5ecf4095
Ali <aliadnani056@gmail.com>
2024-12-09 17:05:15 +0100

    DAO for request

---
+++ src/main/java/org/example/crm/dao/RequestDao.java
@@ -0,0 +1,10 @@
+package org.example.crm.dao;
+
+import org.example.crm.models.Request;
+import java.util.List;
+
+public interface RequestDao {
+    List<Request> showRequests();
+    boolean acceptRequest(String demandeId, String leadId, String loyaltyCardId);
+    boolean rejectRequest(String demandeId, String leadId, String loyaltyCardId);
+}
