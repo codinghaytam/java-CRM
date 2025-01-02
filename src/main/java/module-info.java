087aaca9d85fd04cdcfc62097cd1e93fd698ffce
Ali <aliadnani056@gmail.com>
2025-01-02 22:47:15 +0100

    main

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,11 +4,11 @@ module org.example.crm {
     requires java.sql;
     requires static lombok;
     requires jbcrypt;
-	requires org.junit.jupiter.api;
-	requires org.mockito;
-
+    requires org.junit.jupiter.api;
+    requires org.mockito;
 
     opens org.example.crm to javafx.fxml;
+    opens org.example.crm.dao.impl to org.junit.platform.commons;  // <-- Open this package to JUnit
     exports org.example.crm;
     exports org.example.crm.controllers;
     opens org.example.crm.controllers to javafx.fxml;
